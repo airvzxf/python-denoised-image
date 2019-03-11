0707d91b7df5d31dfc32d64b7770e91f78ef6b3d
Israel Roldan <israel.alberto.rv@gmail.com>
2019-03-11 12:52:33 -0600

    Added a fake comment to check if the hub docker build automatically and show the changes when pull the docker repository airvzxf/...

--- src/__main__.py
+++ src/__main__.py
@@ -5,3 +5,4 @@ This source code shows how to de-noise an image using the median filter.
 
 Here is a good place to import the package denoised_image and start to de-noising an image.
 """
+# Delete me, pls
